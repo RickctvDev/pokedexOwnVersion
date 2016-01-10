074c8b5014074b7a2d204b38bf90100bf15bf872
Rick C <antmathews@hotmail.com>
2016-01-10 23:02:52 +0100

    Initial Commit

---
+++ Pokedex My Attempt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokedex My Attempt
+//
+//  Created by Rick Crane on 10/01/2016.
+//  Copyright © 2016 Rick Crane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
